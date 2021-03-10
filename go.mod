70d23c65de350ff4005ca78b39bf55d1a6d530f8
arikui1911 <arikui.ruby@gmail.com>
2021-03-11 04:51:02 +0900

    [WIP] tokenizer with strscan

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/arikui1911/gosc
 
 go 1.13
+
+require github.com/arikui1911/strscan v0.0.0-20210310190038-8fa7ae38b1c9 // indirect
--- tokenizer.go
+++ tokenizer.go
@@ -1,134 +1,73 @@
 package gosc
 
 import (
-	"bufio"
-	"fmt"
-	"io"
 	"regexp"
+
+	"github.com/arikui1911/strscan"
 )
 
 type Token struct {
-	Tag    TokenTag
-	Value  string
-	Lineno int
-	Column int
+	Tag         TokenTag
+	Value       string
+	FirstLineno int
+	FirstColumn int
+	LastLineno  int
+	LastColumn  int
 }
 
 type Tokenizer struct {
-	src    io.Reader
-	lineno int
-	column int
-	// runeBuf []rune
+	src     string
+	tokenCh chan Token
+	errCh   chan error
 }
 
-func NewTokenizer(src io.Reader) *Tokenizer {
-	return &Tokenizer{src: src}
-}
-
-func (t *Tokenizer) scan() error {
-	scanner := bufio.NewScanner(t.src)
-	t.lineno = 1
-	t.column = 1
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-		t.lineno++
-	}
-	return scanner.Err()
-}
-
-var spacesRe = regexp.MustCompile(`\A\s+`)
-
-func scanInitial(t *Tokenizer, src string) {
-	switch {
-	case spacesRe.MatchString(src):
+func NewTokenizer(src string) *Tokenizer {
+	return &Tokenizer{
+		src:     src,
+		tokenCh: make(chan Token),
+		errCh:   make(chan error),
 	}
 }
 
-/*
-func (t *Tokenizer) getc() (c rune, err error) {
-	if len(t.runeBuf) > 0 {
-		c = t.runeBuf[len(t.runeBuf)-1]
-		t.runeBuf = t.runeBuf[:len(t.runeBuf)-1]
-	} else {
-		c, _, err = t.src.ReadRune()
+func (t *Tokenizer) NextToken() (tok Token, err error) {
+	select {
+	case tok = <-t.tokenCh:
+	case err = <-t.errCh:
 	}
 	return
 }
 
-func (t *Tokenizer) ungetc(c rune) {
-	t.runeBuf = append(t.runeBuf, c)
+func emit(t *Tokenizer, s *strscan.StringScanner, tag TokenTag, val string, begPos int) {
+	fl, fc := s.LinenoAndColumn(begPos)
+	ll, lc := s.LinenoAndColumn(s.Pos() - 1)
+	t.tokenCh <- Token{tag, val, fl, fc, ll, lc}
 }
 
-type tokenState int
+var spacesRe = regexp.MustCompile(`\s+`)
+var charsRe = regexp.MustCompile(`[().']`)
+var atomRe = regexp.MustCompile(`[^\s;"()']+`)
 
-const (
-	invalidState tokenState = iota
-	initialState
-	commentState
-	postSharpState
-	regionCommentState
-	postRegionCommentSharpState
-	symbolState
-	stringState
-	atomState
-)
+var charsTable = map[string]TokenTag{
+	"(": LeftParanToken,
+	")": RightParenToken,
+	".": DotToken,
+	"'": QuoteToken,
+}
 
-func (t *Tokenizer) NextToken() (token Token, retErr error) {
-	state := initialState
-	buf := []rune{}
-	regionCommentNests := 0
-
-	for {
-		c, err := t.getc()
-		if err == io.EOF {
-			token.Tag = EOFToken
-			token.Lineno = t.lineno
-			token.Column = t.column
-			return
-		}
-		if err != nil {
-			retErr = err
-			return
-		}
-
-		switch state {
-		case initialState:
-			if unicode.IsSpace(c) {
-				continue
-			} else if c == ';' {
-				state = commentState
-				continue
-			} else if c == '#' {
-				state = postSharpState
-				continue
-			}
-		case commentState:
-			if c == '\n' {
-				state = initialState
-			}
-		case postSharpState:
-			if c == '|' {
-				state = regionCommentState
-				regionCommentNests++
-				continue
-			}
-			buf = append(buf, '#')
-			buf = append(buf, c)
-			state = atomState
-		case regionCommentState:
-			if c == '#' {
-				state = postRegionCommentSharpState
-			}
-		case postRegionCommentSharpState:
-			if c == '|' {
-				regionCommentNests--
-				if regionCommentNests == 0 {
-					state = initialState
-					continue
-				}
-			}
-			state = regionCommentState
+func scan(t *Tokenizer) error {
+	s := strscan.New(t.src)
+	for !s.IsEOF() {
+		p := s.Pos()
+		switch {
+		case s.Scan(spacesRe):
+			continue
+		case s.Scan(charsRe):
+			emit(t, s, charsTable[s.Matched()], s.Matched(), p)
+		case s.Scan(atomRe):
+			emit(t, s, AtomToken, s.Matched(), p)
+		default:
+			panic("must not happen")
 		}
 	}
+	return nil
 }
-*/
